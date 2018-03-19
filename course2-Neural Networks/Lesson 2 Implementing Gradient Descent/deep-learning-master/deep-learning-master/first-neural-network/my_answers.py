302eb277dfae63c1ac0c34c59fe929a9e88686ad
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-03-18 23:09:10 -0400

    staring 3/19 course

--- course2-Neural Networks/Lesson 2 Implementing Gradient Descent/deep-learning-master/deep-learning-master/first-neural-network/my_answers.py
+++ course2-Neural Networks/Lesson 2 Implementing Gradient Descent/deep-learning-master/deep-learning-master/first-neural-network/my_answers.py
@@ -135,7 +135,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 2000
-learning_rate = 0.1
-hidden_nodes = 8
+iterations = 3000
+learning_rate = 0.6
+hidden_nodes = 20
 output_nodes = 1
